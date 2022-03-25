dff2d2009f9577d805bb17db7f600963ee382b89
towe-q <99953661+towe-q@users.noreply.github.com>
2022-03-25 14:22:35 +0300

    Add files via upload

---
+++ libraryproject/main.c
@@ -0,0 +1,26 @@
+/*
+Project county library
+Author: Ryan towett
+Date: 02/03/2022
+compiler: GNU GCC
+Language: C
+License: MIT
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\County Library\n");
+    printf("Welcome To Our Library\n");
+    printf("What would you like to do?\n");
+    printf("1. Add new user.\n");
+    printf("2. Add new book.\n");
+    printf("3. Modify a user details.\n");
+    printf("4. Clear user.\n");
+    printf("5. Modify book details.\n");
+    printf("6. Delete a book\n");
+    printf("7. Delete user.\n");
+    printf("\n");
+    return 0;
+}
